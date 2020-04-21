eec7128e6e3473bf5242ce55f753b8810016cf2a
P <anderson.pablo02@gmail.ccom>
2020-04-21 02:07:26 -0300

    up validador cpf cnpj
    Têm 2 main.js , o primeiro fiz as funcões independetes para validadar cada uma, na segunda  como muitos procedimentos  se repetia   tentei usar  as mesmas funçtem 2 main.js , o primeiro fiz as funções independentes para validar cada uma, na segunda como muitos procedimentos se repetia tentei usar as mesmas funções para validar os doisões  para validar os dois

---
+++ desafio01-Anderson-Pablo-andersonzeroone/main.js
@@ -0,0 +1,106 @@
+function pegarCod(){
+
+  let pegarCod = (document.querySelector(".input").value);
+  let cod = pegarCod.replace(/([\u0300-\u036f]|[^0-9])/g, '');
+  
+  if( cod === ''){ 
+      alert('Preencha o campo')
+  }else if(cod.length !== 11 && cod.length !== 14){
+    alert('Deve conter 11 numeros para CPF ou 14 para CNPJ.');
+  
+  }else{
+      cod.length == 11 ?  validarCpf(codInArray(cod)): validarCnpj(codInArray(cod))
+  }
+}
+
+function codInArray(cod){
+let codArray = []
+
+for( let i = 0; i < cod.length; i++ ){
+  codArray.push(parseInt(cod.substr(i,1)));
+}
+
+  return codArray;
+}
+
+function validarCpf(cpfArray){
+
+let cpfAr=cpfArray;
+
+let primeiroDv = cpfAr[9];
+let primeiraVerificacao =  verificarDv(calDigito(10,cpfAr),primeiroDv);
+
+if(primeiraVerificacao === true){
+
+  let segundoDv = cpfAr[10];
+  let segundaVerificacao =  verificarDv(calDigito(11,cpfAr),segundoDv, 1);
+
+  segundaVerificacao === true ? document.getElementById("messege").innerHTML = `CPF valido!`:  
+                                document.getElementById("messege").innerHTML = `CPF Invalido!`             
+
+}else{
+  document.getElementById("messege").innerHTML = `CPF Invalido `;
+}
+}
+
+function calDigito(indice,cpfAr){
+let digito = 0;
+let soma = 0;
+
+for(i=indice; i>=2; i--){
+  soma = soma + (cpfAr[digito] * i);
+  digito++
+}
+
+let restoCal = (soma*10) % 11;
+return  restoCal;
+
+}
+
+function verificarDv(resto,digitoVerificador){
+  if(resto === 10 || resto ===11) resto = 0;
+  return resto === digitoVerificador ? true : false 
+}
+
+function validarCnpj(codArray){
+
+let codAr = codArray;
+let primeiroDvCnpj = codAr[12]
+let primeiroDigitoV = digitoDvCnpj( calcularDvCnpj(1,codAr),primeiroDvCnpj)
+  
+if( primeiroDigitoV == true){
+
+  let segundoDv = codAr[13];
+  let segundaVerificacao = digitoDvCnpj( calcularDvCnpj(2,codAr),segundoDv ) 
+     segundaVerificacao === true ? document.getElementById("messege").innerHTML = `CNPJ valido!`:
+                                   document.getElementById("messege").innerHTML = `CNPJ Invalido!`             
+    
+}else{
+  document.getElementById("messege").innerHTML = `CNPJ Invalido `;
+}
+
+
+}
+function  calcularDvCnpj(dv,codAr ){
+let array = [6,7,8,9,2,3,4,5,6,7,8,9]
+let soma = 0
+
+if(dv ==2) array.unshift(5)
+
+for( let  i = 0; i <array.length; i++){
+  soma = soma +(codAr[i] * array[i])
+}
+
+let resto = soma % 11
+return resto 
+}
+function digitoDvCnpj( resto,digitoCnpj){
+  if (resto == 10) resto = 0
+  let res =  resto === digitoCnpj ? true : false 
+  return res;
+}
+
+let apagar = () => {
+  document.querySelector(".input").value = ''
+  document.getElementById("messege").innerHTML = ''
+}
---
+++ desafio01-Anderson-Pablo-andersonzeroone/main2.js
@@ -0,0 +1,98 @@
+function pegarCod(){
+
+  let pegarCod = (document.querySelector(".input").value);
+  let cod = pegarCod.replace(/([\u0300-\u036f]|[^0-9])/g, '');
+  
+  if( cod === ''){ 
+      alert('Preencha o campo')
+  }else if(cod.length !== 11 && cod.length !== 14){
+    alert('Deve conter 11 numeros para CPF ou 14 para CNPJ.');
+  
+  }else{
+      cod.length == 11 ?  validar(codInArray(cod),9,10): validar(codInArray(cod),12,13)
+  }
+}
+
+function codInArray(cod){
+let codArray = []
+
+for( let i = 0; i < cod.length; i++ ){
+  codArray.push(parseInt(cod.substr(i,1)));
+}
+
+  return codArray;
+}
+
+function validar(codArray, priDv, segDv){
+
+let cfp_cnpg=codArray;
+let tipo= 1
+let doc = 'CPF'
+
+let calPrimDv = calcularDvCpf(10,cfp_cnpg)
+let calSegunDv = calcularDvCpf(11,cfp_cnpg)
+
+if(cfp_cnpg.length == 14){
+  tipo = 2
+  doc = 'CNPJ'
+  calPrimDv = calcularDvCnpj(1,cfp_cnpg)
+  calSegunDv = calcularDvCnpj(2,cfp_cnpg)
+ 
+} 
+
+let primeiroDv = cfp_cnpg[priDv];
+let primeiraVerificacao =  verificarDv(calPrimDv,primeiroDv, tipo);
+
+if(primeiraVerificacao === true){
+
+  let segundoDv = cfp_cnpg[segDv];
+  let segundaVerificacao =  verificarDv(calSegunDv,segundoDv, tipo);
+
+  segundaVerificacao === true ? document.getElementById("messege").innerHTML = `${doc} valido!`:  
+                                document.getElementById("messege").innerHTML = `${doc} Invalido!`             
+
+}else{
+  document.getElementById("messege").innerHTML = `CPF Invalido `;
+}
+}
+
+function calcularDvCpf(indice,cfp_cnpg){
+let digito = 0;
+let soma = 0;
+
+for(i=indice; i>=2; i--){
+  soma = soma + (cfp_cnpg[digito] * i);
+  digito++
+}
+
+let restoCal = (soma*10) % 11;
+return  restoCal;
+
+}
+
+function  calcularDvCnpj(dv,cfp_cnpg ){
+let array = [6,7,8,9,2,3,4,5,6,7,8,9]
+let soma = 0
+
+if(dv ==2) array.unshift(5)
+for( let  i = 0; i <array.length; i++){
+  soma = soma +(cfp_cnpg[i] * array[i])
+}
+
+let resto = soma % 11
+return resto 
+}
+
+function verificarDv(resto,digitoVerificador, tipo){
+if( resto == 11 && tipo == 1){
+  resto = 0 
+  return resto === digitoVerificador ? true : false 
+}    
+if(resto === 10) resto = 0;
+  return resto === digitoVerificador ? true : false 
+}
+
+let apagar = () => {
+  document.querySelector(".input").value = ''
+  document.getElementById("messege").innerHTML = ''
+}
