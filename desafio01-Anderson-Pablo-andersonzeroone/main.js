7ef999d345028f4dbb228c13977fbc5aa8ad579c
P <anderson.pablo02@gmail.ccom>
2020-04-23 18:49:19 -0300

    correção identação

--- desafio01-Anderson-Pablo-andersonzeroone/main.js
+++ desafio01-Anderson-Pablo-andersonzeroone/main.js
@@ -14,46 +14,46 @@ function pegarCod(){
 }
 
 function codInArray(cod){
-let codArray = []
+  let codArray = []
 
-for( let i = 0; i < cod.length; i++ ){
-  codArray.push(parseInt(cod.substr(i,1)));
-}
+  for( let i = 0; i < cod.length; i++ ){
+    codArray.push(parseInt(cod.substr(i,1)));
+  }
 
   return codArray;
 }
 
 function validarCpf(cpfArray){
 
-let cpfAr=cpfArray;
+  let cpfAr=cpfArray;
 
-let primeiroDv = cpfAr[9];
-let primeiraVerificacao =  verificarDv(calDigito(10,cpfAr),primeiroDv);
+  let primeiroDv = cpfAr[9];
+  let primeiraVerificacao =  verificarDv(calDigito(10,cpfAr),primeiroDv);
 
-if(primeiraVerificacao === true){
+  if(primeiraVerificacao === true){
 
-  let segundoDv = cpfAr[10];
-  let segundaVerificacao =  verificarDv(calDigito(11,cpfAr),segundoDv, 1);
+    let segundoDv = cpfAr[10];
+    let segundaVerificacao =  verificarDv(calDigito(11,cpfAr),segundoDv, 1);
 
-  segundaVerificacao === true ? document.getElementById("messege").innerHTML = `CPF valido!`:  
-                                document.getElementById("messege").innerHTML = `CPF Invalido!`             
+    segundaVerificacao === true ? document.getElementById("messege").innerHTML = `CPF valido!`:  
+                                  document.getElementById("messege").innerHTML = `CPF Invalido!`             
 
-}else{
-  document.getElementById("messege").innerHTML = `CPF Invalido `;
-}
+  }else{
+    document.getElementById("messege").innerHTML = `CPF Invalido `;
+  }
 }
 
 function calDigito(indice,cpfAr){
-let digito = 0;
-let soma = 0;
+  let digito = 0;
+  let soma = 0;
 
-for(i=indice; i>=2; i--){
-  soma = soma + (cpfAr[digito] * i);
-  digito++
-}
+  for(i=indice; i>=2; i--){
+    soma = soma + (cpfAr[digito] * i);
+    digito++
+  }
 
-let restoCal = (soma*10) % 11;
-return  restoCal;
+  let restoCal = (soma*10) % 11;
+  return  restoCal;
 
 }
 
@@ -64,35 +64,35 @@ function verificarDv(resto,digitoVerificador){
 
 function validarCnpj(codArray){
 
-let codAr = codArray;
-let primeiroDvCnpj = codAr[12]
-let primeiroDigitoV = digitoDvCnpj( calcularDvCnpj(1,codAr),primeiroDvCnpj)
-  
-if( primeiroDigitoV == true){
-
-  let segundoDv = codAr[13];
-  let segundaVerificacao = digitoDvCnpj( calcularDvCnpj(2,codAr),segundoDv ) 
-     segundaVerificacao === true ? document.getElementById("messege").innerHTML = `CNPJ valido!`:
-                                   document.getElementById("messege").innerHTML = `CNPJ Invalido!`             
+  let codAr = codArray;
+  let primeiroDvCnpj = codAr[12]
+  let primeiroDigitoV = digitoDvCnpj( calcularDvCnpj(1,codAr),primeiroDvCnpj)
     
-}else{
-  document.getElementById("messege").innerHTML = `CNPJ Invalido `;
-}
+  if( primeiroDigitoV == true){
+
+    let segundoDv = codAr[13];
+    let segundaVerificacao = digitoDvCnpj( calcularDvCnpj(2,codAr),segundoDv ) 
+      segundaVerificacao === true ? document.getElementById("messege").innerHTML = `CNPJ valido!`:
+                                    document.getElementById("messege").innerHTML = `CNPJ Invalido!`             
+      
+  }else{
+    document.getElementById("messege").innerHTML = `CNPJ Invalido `;
+  }
 
 
 }
 function  calcularDvCnpj(dv,codAr ){
-let array = [6,7,8,9,2,3,4,5,6,7,8,9]
-let soma = 0
+  let array = [6,7,8,9,2,3,4,5,6,7,8,9]
+  let soma = 0
 
-if(dv ==2) array.unshift(5)
+  if(dv ==2) array.unshift(5)
 
-for( let  i = 0; i <array.length; i++){
-  soma = soma +(codAr[i] * array[i])
-}
+  for( let  i = 0; i <array.length; i++){
+    soma = soma +(codAr[i] * array[i])
+  }
 
-let resto = soma % 11
-return resto 
+  let resto = soma % 11
+  return resto 
 }
 function digitoDvCnpj( resto,digitoCnpj){
   if (resto == 10) resto = 0
--- desafio01-Anderson-Pablo-andersonzeroone/main2.js
+++ desafio01-Anderson-Pablo-andersonzeroone/main2.js
@@ -25,72 +25,72 @@ for( let i = 0; i < cod.length; i++ ){
 
 function validar(codArray, priDv, segDv){
 
-let cfp_cnpg=codArray;
-let tipo= 1
-let doc = 'CPF'
+  let cfp_cnpg=codArray;
+  let tipo= 1
+  let doc = 'CPF'
 
-let calPrimDv = calcularDvCpf(10,cfp_cnpg)
-let calSegunDv = calcularDvCpf(11,cfp_cnpg)
+  let calPrimDv = calcularDvCpf(10,cfp_cnpg)
+  let calSegunDv = calcularDvCpf(11,cfp_cnpg)
 
-if(cfp_cnpg.length == 14){
-  tipo = 2
-  doc = 'CNPJ'
-  calPrimDv = calcularDvCnpj(1,cfp_cnpg)
-  calSegunDv = calcularDvCnpj(2,cfp_cnpg)
+  if(cfp_cnpg.length == 14){
+    tipo = 2
+    doc = 'CNPJ'
+    calPrimDv = calcularDvCnpj(1,cfp_cnpg)
+    calSegunDv = calcularDvCnpj(2,cfp_cnpg)
  
-} 
+  } 
 
-let primeiroDv = cfp_cnpg[priDv];
-let primeiraVerificacao =  verificarDv(calPrimDv,primeiroDv, tipo);
+  let primeiroDv = cfp_cnpg[priDv];
+  let primeiraVerificacao =  verificarDv(calPrimDv,primeiroDv, tipo);
 
-if(primeiraVerificacao === true){
+  if(primeiraVerificacao === true){
 
-  let segundoDv = cfp_cnpg[segDv];
-  let segundaVerificacao =  verificarDv(calSegunDv,segundoDv, tipo);
+    let segundoDv = cfp_cnpg[segDv];
+    let segundaVerificacao =  verificarDv(calSegunDv,segundoDv, tipo);
 
-  segundaVerificacao === true ? document.getElementById("messege").innerHTML = `${doc} valido!`:  
-                                document.getElementById("messege").innerHTML = `${doc} Invalido!`             
+    segundaVerificacao === true ? document.getElementById("messege").innerHTML = `${doc} valido!`:  
+                                  document.getElementById("messege").innerHTML = `${doc} Invalido!`             
 
-}else{
+  }else{
   document.getElementById("messege").innerHTML = `CPF Invalido `;
-}
+  }
 }
 
 function calcularDvCpf(indice,cfp_cnpg){
-let digito = 0;
-let soma = 0;
+  let digito = 0;
+  let soma = 0;
 
-for(i=indice; i>=2; i--){
+  for(i=indice; i>=2; i--){
   soma = soma + (cfp_cnpg[digito] * i);
   digito++
 }
 
-let restoCal = (soma*10) % 11;
-return  restoCal;
+  let restoCal = (soma*10) % 11;
+  return  restoCal;
 
 }
 
 function  calcularDvCnpj(dv,cfp_cnpg ){
-let array = [6,7,8,9,2,3,4,5,6,7,8,9]
-let soma = 0
+  let array = [6,7,8,9,2,3,4,5,6,7,8,9]
+  let soma = 0
 
-if(dv ==2) array.unshift(5)
-for( let  i = 0; i <array.length; i++){
-  soma = soma +(cfp_cnpg[i] * array[i])
+  if(dv ==2) array.unshift(5)
+  for( let  i = 0; i <array.length; i++){
+    soma = soma +(cfp_cnpg[i] * array[i])
 }
 
-let resto = soma % 11
-return resto 
+  let resto = soma % 11
+  return resto 
 }
 
 function verificarDv(resto,digitoVerificador, tipo){
-if( resto == 11 && tipo == 1){
-  resto = 0 
-  return resto === digitoVerificador ? true : false 
-}    
-if(resto === 10) resto = 0;
-  return resto === digitoVerificador ? true : false 
-}
+  if( resto == 11 && tipo == 1){
+    resto = 0 
+    return resto === digitoVerificador ? true : false 
+  }    
+  if(resto === 10) resto = 0;
+    return resto === digitoVerificador ? true : false 
+  }
 
 let apagar = () => {
   document.querySelector(".input").value = ''
